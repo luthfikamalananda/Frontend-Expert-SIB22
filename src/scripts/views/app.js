94c024bb3876e154362c2fc14009dfa95e9cc053
luthfikamalananda <luthfi_kamal_ananda@yahoo.com>
2023-03-02 16:36:40 +0700

    Upload Full Project to Github

---
+++ src/scripts/views/app.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-underscore-dangle */
+import DrawerInitiator from '../utils/drawer-initiator';
+import UrlParser from '../routes/url-parser';
+import routes from '../routes/routes';
+
+class App {
+  constructor({ button, drawer, content }) {
+    this._button = button;
+    this._drawer = drawer;
+    this._content = content;
+
+    this._initialAppShell();
+  }
+
+  _initialAppShell() {
+    DrawerInitiator.init({
+      button: this._button,
+      drawer: this._drawer,
+      content: this._content,
+    });
+  }
+
+  async renderPage() {
+    const url = UrlParser.parseActiveUrlWithCombiner();
+    console.log('url', url);
+    const page = routes[url];
+    console.log('page', page);
+    this._content.innerHTML = await page.render();
+    await page.afterRender();
+    const skipLinkElem = document.querySelector('.skip-link');
+    skipLinkElem.addEventListener('click', (event) => {
+      event.preventDefault();
+      document.querySelector('#main').focus();
+    });
+  }
+}
+
+export default App;
